462309d5d4293689ce263bc4e6636d4b5a6aa3cb
lgrayes <lgrayes@gmail.com>
2020-12-13 18:55:19 -0600

    Test bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,44 @@
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
+// Use D3 fetch to read the JSON file
+// The data from the JSON file is arbitrarily named importedData as the argument
+d3.json("samples.json").then((importedData) => {
+  // console.log(importedData);
+  var samples = importedData;
+
+  // Sort the data array using the greekSearchResults value
+  data.sort(function(a, b) {
+    return parseFloat(b.otu_ids) - parseFloat(a.otu_ids);
+  });
+
+  // Slice the first 10 objects for plotting
+  data = data.slice(0, 10);
+
+  // Reverse the array due to Plotly's defaults
+  data = data.reverse();
+
+  // Trace1 for the Greek Data
+  var trace1 = {
+    x: data.map(row => row.sample_values),
+    y: data.map(row => row.otu_ids),
+    text: data.map(row => row.otu_labels),
+    name: "OTU Labels",
+    type: "bar",
+    orientation: "h"
+  };
+
+  // data
+  var chartData = [trace1];
+
+  // Apply the group bar mode to the layout
+  var layout = {
+    title: "TOP 10 OTU LABELS",
+    margin: {
+      l: 100,
+      r: 100,
+      t: 100,
+      b: 100
+    }
+  };
+
+  // Render the plot to the div tag with id "plot"
+  Plotly.newPlot("plot", chartData, layout);
+});

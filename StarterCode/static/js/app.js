1c79b93cbfa0d857c3fb5182530b44826356448a
lgrayes <lgrayes@gmail.com>
2020-12-20 03:13:46 -0600

    Information box now working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -39,8 +39,8 @@ console.log("Data Promise: ", dataPromise)
 // LOAD NAMES ARRAY TO DROPDOWN
 // **********************
 
-function init() {
-
+function init() 
+{
   var dropdown = d3.select("#selDataset");
 
     d3.json("samples.json").then(function(data) 
@@ -50,20 +50,23 @@ function init() {
           dropdown.append("option").text(name).property("value");
         }
       );
-  
-    //   var sampleIDNames = data.names;
-  
-    //   console.log("IDs ", sampleIDNames);
-  
-    //   sampleIDNames.forEach((sample) => {
-  
-    //     selector.append("option").text(sample).property("value");
-    //   })
-  
-    //   buildBarPlot(sampleIDNames[0]);
-    })
+    }
+  )
 }
+
 init();
+  
+      // var sampleIDNames = data.names;
+  
+      // console.log("IDs ", sampleIDNames);
+  
+      // sampleIDNames.forEach((sample) => {
+  
+      //   selector.append("option").text(sample).property("value");
+      // })
+  
+      // buildBarPlot(sampleIDNames[0]);
+
 
 // **********************
 // WHEN SELECTION IS MADE AT THE DROPDOWN
@@ -71,77 +74,133 @@ init();
 
 function optionChanged(id) {
 
-  getPlot(id);
-  getInfo(id);
+  getGraphs(id);
+  getDemographicInfo(id);
 };
 
 // **********************
 // HORIZONTAL BAR GRAPH
 // **********************
 
-function getPlot(id) 
+function getGraphs(id) 
 {
-  d3.json("samples.json").then(function(data)
-  {
-    var samples = data.samples.filter(function(samplesdata)
+  d3.json("samples.json").then
+  (
+    function(data)
     {
-      return samplesdata.id === id;
-    }
-    )[0]
+      var samples = data.samples.filter(function(samplesdata)
+        {
+          return samplesdata.id === id;
+        }
+      )[0]
 
-    console.log(samples);
+      console.log(samples);
     
-    // slice
-    var sampleValues = samples.sample_values.slice(0, 10).reverse();
+      // slice
+      var sampleValues = samples.sample_values.slice(0, 10).reverse();
 
-    var idValues = (samples.otu_ids.slice(0,10)).reverse();
+      var idValues = (samples.otu_ids.slice(0,10)).reverse();
 
-    var idOtu = idValues.map(function(datavalue)
+      var idOtu = idValues.map(function(datavalue)
+        {
+          return "OTU" + datavalue
+        }
+      )
+
+      console.log(`OTU IDS: ${idOtu}`)
+
+      // labels
+      var labels = samples.otu_labels.slice(0,10);
+
+      console.log(`Sample Values: ${sampleValues}`)
+      console.log(`ID Values: ${idValues}`)
+
+      // trace
+      var trace1 = 
       {
-        return "OTU" + datavalue
-      }
-    )
+        x: sampleValues,
+        y: idOtu,
+        type: "bar",
+        orientation: "h",
+      };
 
-    console.log(`OTU IDS: ${idOtu}`)
+      // layout
+      var data1 = [trace1];
 
-    // labels
-    var labels = samples.otu_labels.slice(0,10);
-
-    console.log(`Sample Values: ${sampleValues}`)
-    console.log(`ID Values: ${idValues}`)
-
-    // trace
-    var trace1 = 
-    {
-      x: sampleValues,
-      y: idOtu,
-      type: "bar",
-      orientation: "h",
-    };
-
-    // layout
-    var data1 = [trace1];
-
-    var layout1 = 
-    {
-      title: "Top 10 OTU",
-      yaxis:
+      var layout1 = 
       {
-        tickmode: "linear",
-      }
-    };
+        title: "Top 10 OTU",
+        yaxis:
+        {
+          tickmode: "linear",
+        }
+      };
 
-    Plotly.newPlot("bar", data1, layout1);
-  }
-  )
+      Plotly.newPlot("bar", data1, layout1);
+    
+
+      // **********************
+      // BUBBLES - trace2
+      // **********************
+
+      var trace2 = 
+      {
+        x: samples.otu_ids,
+        y: samples.sample_values,
+        mode: "markers",
+        marker:
+        {
+          size: samples.sample_values,
+          color: samples.otu_ids
+        }
+      };
+
+      var layout2 =
+      {
+        xaxis: {title: "OTU ID"},
+      };
+
+      var data2 = [trace2];
+
+      Plotly.newPlot("bubble", data2, layout2);
+    }
+  );
 }
 
-// **********************
-// BUBBLES - trace2
-// **********************
+function getDemographicInfo(idstring)
+{
+  var id = +idstring
 
-// **********************
-// OPTIONAL: GAUGE
-// **********************
+  d3.json("samples.json").then(function(data)
+    {
+      var DisplayData = data.metadata;
 
+      console.log(id, DisplayData)
 
+      var resultArray = DisplayData.filter(function(meta)
+        {
+          if (meta.id === id)
+          {
+            return true;
+          }
+          else
+          {
+            return false;
+          }
+        }
+      )
+      var result = resultArray[0]
+      console.log(result)
+
+      var InfoMetadata = d3.select("#sample-metadata");
+
+      InfoMetadata.html("");
+
+      Object.entries(result).forEach((key) => 
+        {
+          InfoMetadata.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+        }
+      )
+    }
+  )
+}
